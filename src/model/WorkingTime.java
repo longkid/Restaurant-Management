f8760cc5fb303ada3de26f0385a030aecd084ed2
Lam Ho <hotienlam@gmail.com>
2011-06-03 11:44:42 +0700

    Modify PositionTitle and WorkingTime enumeration

--- src/model/WorkingTime.java
+++ src/model/WorkingTime.java
@@ -4,5 +4,7 @@ package model;
  * @author Tu Thi Xuan Hien
  */
 public enum WorkingTime {
-	DAY_SHIFT, NIGHT_SHIFT
+	DAY_SHIFT, 		// 9 am - 4 pm
+	NIGHT_SHIFT, 	// 4 pm - 11 pm
+	OFFICE_HOURS	// 8 am - 5 pm
 }
