22c6829b5284865550f0ef23b93c8426f39b4944
Lam Ho <hotienlam@gmail.com>
2011-06-04 15:39:14 +0700

    Change name: PayRollFrame->PayrollFrame, ViewReportFrame->WorkingDaysReportFrame, remove redundant ViewReport class

--- src/view/PayrollFrame.java
+++ src/view/PayrollFrame.java
@@ -1,6 +1,5 @@
 package view;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -12,31 +11,34 @@ import java.awt.Font;
 
 import javax.swing.ButtonGroup;
 import javax.swing.SwingConstants;
-import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JRadioButton;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import java.awt.Button;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 
-public class PayRollFrame extends JFrame implements ActionListener {
+/*
+ * @author Tu Thi Xuan Hien
+ */
+public class PayrollFrame extends JFrame implements ActionListener {
 
-	private JPanel contentPane;
-	private JTextField txtMonth=new JTextField();
-	private JTextField txtYear=new JTextField();
+	private JTextField txtMonth = new JTextField();
+	private JTextField txtYear = new JTextField();
 	private JButton btnView = new JButton("View");
 	private JButton btnClose = new JButton("Close");
 	private JComboBox cboSelectEmployee = new JComboBox();
-	private JRadioButton rdbtnViewAllEmployee = new JRadioButton("View PayRoll report of all Employees");
-	private JRadioButton rdbtnViewOneEmployee = new JRadioButton("View PayRoll report of employee");
+	private JRadioButton rdbtnViewAllEmployees = new JRadioButton(
+			"View report of all Employees");
+	private JRadioButton rdbtnViewOneEmployee = new JRadioButton(
+			"View report of employee");
+
+	// 2011-06-03 LH: This method is only used to test the functionality of this
+	// frame. It'll be deleted at final version.
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					PayRollFrame frame = new PayRollFrame();
+					PayrollFrame frame = new PayrollFrame();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -48,75 +50,67 @@ public class PayRollFrame extends JFrame implements ActionListener {
 	/**
 	 * Create the frame.
 	 */
-	public PayRollFrame() {
-		setTitle("Payroll");
+	public PayrollFrame() {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 635, 349);
-		contentPane = new JPanel();
+		JPanel contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblPay = new JLabel("PAYROLL");
 		lblPay.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPay.setFont(new Font("Tahoma", Font.BOLD, 25));
 		lblPay.setBounds(10, 0, 607, 52);
 		contentPane.add(lblPay);
-		
+
 		JLabel lblMonth = new JLabel("Month");
 		lblMonth.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblMonth.setBounds(63, 63, 65, 28);
 		contentPane.add(lblMonth);
-		
+
 		JLabel lblYear = new JLabel("Year");
 		lblYear.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblYear.setBounds(354, 63, 65, 28);
 		contentPane.add(lblYear);
-		
-		
+
 		txtMonth.setBounds(135, 63, 138, 28);
 		contentPane.add(txtMonth);
 		txtMonth.setColumns(10);
-		
-		
+
 		txtYear.setBounds(414, 63, 138, 28);
 		contentPane.add(txtYear);
 		txtYear.setColumns(10);
-		
-		
+
 		btnView.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnView.setBounds(194, 255, 115, 32);
 		contentPane.add(btnView);
-		
-		
+
 		btnClose.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnClose.setBounds(354, 255, 106, 32);
 		btnClose.addActionListener(this);
 		contentPane.add(btnClose);
-		
-		
+
 		cboSelectEmployee.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		cboSelectEmployee.setBounds(398, 171, 126, 28);
+		cboSelectEmployee.setBounds(306, 172, 246, 28);
 		contentPane.add(cboSelectEmployee);
-		
-		
-		rdbtnViewAllEmployee.setBounds(62, 127, 273, 28);
-		contentPane.add(rdbtnViewAllEmployee);
-		
-		
-		rdbtnViewOneEmployee.setBounds(63, 174, 233, 25);
+
+		rdbtnViewAllEmployees.setBounds(63, 127, 235, 28);
+		contentPane.add(rdbtnViewAllEmployees);
+
+		rdbtnViewOneEmployee.setBounds(63, 174, 235, 25);
 		contentPane.add(rdbtnViewOneEmployee);
-		
-		ButtonGroup group=new ButtonGroup();
-		group.add(rdbtnViewAllEmployee);
+
+		ButtonGroup group = new ButtonGroup();
+		group.add(rdbtnViewAllEmployees);
 		group.add(rdbtnViewOneEmployee);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		Object source = arg0.getSource();
-		
+
 		if (source == btnClose) {
 			dispose();
 		}
--- src/view/WorkingDaysReportFrame.java
+++ src/view/WorkingDaysReportFrame.java
@@ -1,6 +1,5 @@
 package view;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -18,27 +17,30 @@ import javax.swing.JComboBox;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 
-public class ViewReport extends JFrame implements ActionListener{
+/*
+ * @author Tu Thi Xuan Hien
+ */
+public class WorkingDaysReportFrame extends JFrame implements ActionListener {
 
-	private JPanel contentPane;
-	private JTextField txtMonth=new JTextField();
+	private JTextField txtMonth = new JTextField();
 	private JTextField txtFrom = new JTextField();
 	private JTextField txtTo = new JTextField();
 	private JTextField txtSummarizeMonth = new JTextField();
-	private JRadioButton rdbtnEmployeeName = new JRadioButton("Employee name");
+	private JRadioButton rdbtnEmployeeName = new JRadioButton("Employee");
 	private JComboBox cboEmployeeName = new JComboBox();
 	private JRadioButton rdbtnFrom = new JRadioButton("From");
-	private JRadioButton rdbtnSummarize = new JRadioButton("Summarize the working day for all in the month");
-	private JButton btnCloseForm = new JButton("Close form");
+	private JRadioButton rdbtnSummarize = new JRadioButton(
+			"Summarize the working day for all in the month");
+	private JButton btnCloseForm = new JButton("Close");
 	private JButton btnView = new JButton("View");
-	/**
-	 * Launch the application.
-	 */
+
+	// 2011-06-03 LH: This method is only used to test the functionality of this
+	// frame. It'll be deleted at final version.
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					ViewReport frame = new ViewReport();
+					WorkingDaysReportFrame frame = new WorkingDaysReportFrame();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -50,92 +52,82 @@ public class ViewReport extends JFrame implements ActionListener{
 	/**
 	 * Create the frame.
 	 */
-	public ViewReport() {
+	public WorkingDaysReportFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 662, 346);
-		contentPane = new JPanel();
+		JPanel contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JLabel lblViewReport = new JLabel("View Report");
+
+		JLabel lblViewReport = new JLabel("Working-Days Report");
 		lblViewReport.setHorizontalAlignment(SwingConstants.CENTER);
 		lblViewReport.setFont(new Font("Cambria", Font.BOLD, 28));
 		lblViewReport.setBounds(12, 13, 648, 44);
 		contentPane.add(lblViewReport);
-		
-		
+
 		rdbtnEmployeeName.setFont(new Font("Cambria", Font.PLAIN, 16));
 		rdbtnEmployeeName.setBounds(41, 86, 149, 25);
 		contentPane.add(rdbtnEmployeeName);
-		
-		
+
 		cboEmployeeName.setBounds(198, 88, 149, 22);
 		contentPane.add(cboEmployeeName);
-		
-		
+
 		txtMonth.setBounds(447, 86, 137, 24);
 		contentPane.add(txtMonth);
 		txtMonth.setColumns(10);
-		
+
 		JLabel lblMonth = new JLabel("Month");
 		lblMonth.setFont(new Font("Cambria", Font.PLAIN, 16));
 		lblMonth.setBounds(379, 90, 56, 16);
 		contentPane.add(lblMonth);
-		
-		
+
 		rdbtnFrom.setFont(new Font("Cambria", Font.PLAIN, 16));
 		rdbtnFrom.setBounds(41, 134, 127, 25);
 		contentPane.add(rdbtnFrom);
-		
-		
+
 		txtFrom.setBounds(198, 135, 149, 25);
 		contentPane.add(txtFrom);
 		txtFrom.setColumns(10);
-		
+
 		JLabel lblTo = new JLabel("To");
 		lblTo.setFont(new Font("Cambria", Font.PLAIN, 16));
 		lblTo.setBounds(379, 138, 56, 16);
 		contentPane.add(lblTo);
-		
-		
+
 		txtTo.setBounds(447, 135, 137, 25);
 		contentPane.add(txtTo);
 		txtTo.setColumns(10);
-		
-		
+
 		rdbtnSummarize.setFont(new Font("Cambria", Font.PLAIN, 16));
-		rdbtnSummarize.setBounds(43, 184, 374, 25);
+		rdbtnSummarize.setBounds(41, 184, 374, 25);
 		contentPane.add(rdbtnSummarize);
-		
-		
+
 		txtSummarizeMonth.setBounds(447, 185, 137, 25);
 		contentPane.add(txtSummarizeMonth);
 		txtSummarizeMonth.setColumns(10);
-		ButtonGroup group=new ButtonGroup();
+		ButtonGroup group = new ButtonGroup();
 		group.add(rdbtnEmployeeName);
 		group.add(rdbtnFrom);
 		group.add(rdbtnSummarize);
-		
-		
+
 		btnView.setFont(new Font("Cambria", Font.PLAIN, 16));
-		btnView.setBounds(198, 250, 127, 25);		
+		btnView.setBounds(198, 250, 127, 25);
 		contentPane.add(btnView);
-		
-		
+
 		btnCloseForm.setFont(new Font("Cambria", Font.PLAIN, 16));
 		btnCloseForm.setBounds(337, 250, 127, 25);
 		btnCloseForm.addActionListener(this);
 		contentPane.add(btnCloseForm);
-		
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		Object source=arg0.getSource();
-		if(source==btnCloseForm){
+		Object source = arg0.getSource();
+		if (source == btnCloseForm) {
 			dispose();
 		}
-			
+
 	}
 }
