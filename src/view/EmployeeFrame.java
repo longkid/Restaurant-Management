b5dbac30f441984f173869df9766b7aec6408686
Lam Ho <hotienlam@gmail.com>
2011-09-10 23:43:38 +0200

    Edit some labels on EmployeeFrame

--- src/view/EmployeeFrame.java
+++ src/view/EmployeeFrame.java
@@ -62,7 +62,7 @@ public class EmployeeFrame extends JFrame {
 	 */
 	public EmployeeFrame() {
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		setBounds(100, 100, 694, 427);
+		setBounds(100, 100, 725, 427);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -74,157 +74,157 @@ public class EmployeeFrame extends JFrame {
 		lblBasicInformations.setBounds(272, 12, 158, 15);
 		contentPane.add(lblBasicInformations);
 
-		JLabel lblFullName = new JLabel("Full name");
-		lblFullName.setBounds(12, 47, 70, 15);
+		JLabel lblFullName = new JLabel("Full Name (*)");
+		lblFullName.setBounds(12, 47, 138, 15);
 		contentPane.add(lblFullName);
 
 		fullNameTextField = new JTextField();
-		fullNameTextField.setBounds(152, 45, 190, 19);
+		fullNameTextField.setBounds(177, 43, 190, 19);
 		contentPane.add(fullNameTextField);
 		fullNameTextField.setColumns(10);
 
-		JLabel lblNewLabel = new JLabel("Birthday");
-		lblNewLabel.setBounds(360, 47, 70, 15);
+		JLabel lblNewLabel = new JLabel("Birthday (*)");
+		lblNewLabel.setBounds(381, 47, 113, 15);
 		contentPane.add(lblNewLabel);
 
 		birthdayTextField = new JTextField();
 		birthdayTextField.setText(Staff.getDateFormat().toPattern());
-		birthdayTextField.setBounds(478, 45, 202, 19);
+		birthdayTextField.setBounds(503, 43, 202, 19);
 		contentPane.add(birthdayTextField);
 		birthdayTextField.setColumns(10);
 
-		JLabel lblSex = new JLabel("Sex");
+		JLabel lblSex = new JLabel("Sex (*)");
 		lblSex.setBounds(12, 76, 70, 15);
 		contentPane.add(lblSex);
 
 		sexradioMale = new JRadioButton("Male");
 		sexradioMale.setSelected(true);
 		buttonGroup.add(sexradioMale);
-		sexradioMale.setBounds(152, 72, 70, 23);
+		sexradioMale.setBounds(177, 70, 70, 23);
 		contentPane.add(sexradioMale);
 
 		sexradioFemale = new JRadioButton("Female");
 		buttonGroup.add(sexradioFemale);
-		sexradioFemale.setBounds(226, 72, 76, 23);
+		sexradioFemale.setBounds(251, 70, 76, 23);
 		contentPane.add(sexradioFemale);
 
-		JLabel lblEmailAddress = new JLabel("Email address");
-		lblEmailAddress.setBounds(360, 76, 99, 15);
+		JLabel lblEmailAddress = new JLabel("Email");
+		lblEmailAddress.setBounds(381, 76, 99, 15);
 		contentPane.add(lblEmailAddress);
 
 		emailAddressTextField = new JTextField();
-		emailAddressTextField.setBounds(478, 74, 202, 19);
+		emailAddressTextField.setBounds(503, 72, 202, 19);
 		contentPane.add(emailAddressTextField);
 		emailAddressTextField.setColumns(10);
 
-		JLabel lblCellphoneNumber = new JLabel("Cellphone number");
-		lblCellphoneNumber.setBounds(12, 105, 129, 15);
+		JLabel lblCellphoneNumber = new JLabel("Cellphone (*)");
+		lblCellphoneNumber.setBounds(12, 105, 147, 15);
 		contentPane.add(lblCellphoneNumber);
 
 		cellPhoneNumberTextField = new JTextField();
-		cellPhoneNumberTextField.setBounds(152, 103, 190, 19);
+		cellPhoneNumberTextField.setBounds(177, 101, 190, 19);
 		contentPane.add(cellPhoneNumberTextField);
 		cellPhoneNumberTextField.setColumns(10);
 
-		JLabel lblNewLabel_1 = new JLabel("Identity card No");
-		lblNewLabel_1.setBounds(360, 105, 113, 15);
+		JLabel lblNewLabel_1 = new JLabel("ID Card No. (*)");
+		lblNewLabel_1.setBounds(381, 105, 113, 15);
 		contentPane.add(lblNewLabel_1);
 
 		NoIdentityCardTextField = new JTextField();
-		NoIdentityCardTextField.setBounds(478, 103, 202, 19);
+		NoIdentityCardTextField.setBounds(503, 101, 202, 19);
 		contentPane.add(NoIdentityCardTextField);
 		NoIdentityCardTextField.setColumns(10);
 
-		JLabel lblIssueDate = new JLabel("Issued date");
+		JLabel lblIssueDate = new JLabel("Issued Date (*)");
 		lblIssueDate.setBounds(12, 134, 107, 15);
 		contentPane.add(lblIssueDate);
 
 		issueDateText = new JTextField();
 		issueDateText.setText(Staff.getDateFormat().toPattern());
-		issueDateText.setBounds(152, 132, 190, 19);
+		issueDateText.setBounds(177, 130, 190, 19);
 		contentPane.add(issueDateText);
 		issueDateText.setColumns(10);
 
-		JLabel lblIssuePlace = new JLabel("Issued place");
-		lblIssuePlace.setBounds(360, 134, 99, 15);
+		JLabel lblIssuePlace = new JLabel("Issued Place (*)");
+		lblIssuePlace.setBounds(381, 134, 113, 15);
 		contentPane.add(lblIssuePlace);
 
 		issuePlaceTextField = new JTextField();
-		issuePlaceTextField.setBounds(478, 132, 202, 19);
+		issuePlaceTextField.setBounds(503, 130, 202, 19);
 		contentPane.add(issuePlaceTextField);
 		issuePlaceTextField.setColumns(10);
 
-		JLabel lblNewLabel_2 = new JLabel("Permanent address");
-		lblNewLabel_2.setBounds(12, 163, 147, 15);
+		JLabel lblNewLabel_2 = new JLabel("Permanent Address (*)");
+		lblNewLabel_2.setBounds(12, 163, 165, 15);
 		contentPane.add(lblNewLabel_2);
 
 		permanentAddressTexField = new JTextField();
-		permanentAddressTexField.setBounds(152, 163, 528, 19);
+		permanentAddressTexField.setBounds(177, 161, 528, 19);
 		contentPane.add(permanentAddressTexField);
 		permanentAddressTexField.setColumns(10);
 
-		JLabel lblCurrentAddress = new JLabel("Current address");
+		JLabel lblCurrentAddress = new JLabel("Current Address (*)");
 		lblCurrentAddress.setBounds(12, 192, 138, 15);
 		contentPane.add(lblCurrentAddress);
 
 		currentAddressTextField = new JTextField();
-		currentAddressTextField.setBounds(152, 190, 528, 19);
+		currentAddressTextField.setBounds(177, 188, 528, 19);
 		contentPane.add(currentAddressTextField);
 		currentAddressTextField.setColumns(10);
 
-		JLabel lblAcountNo = new JLabel("Account No");
+		JLabel lblAcountNo = new JLabel("Account No. (*)");
 		lblAcountNo.setBounds(12, 221, 107, 15);
 		contentPane.add(lblAcountNo);
 
 		accountNoTextField = new JTextField();
-		accountNoTextField.setBounds(152, 219, 190, 19);
+		accountNoTextField.setBounds(177, 217, 190, 19);
 		contentPane.add(accountNoTextField);
 		accountNoTextField.setColumns(10);
 
-		JLabel lblNameOfBank = new JLabel("Name of bank");
-		lblNameOfBank.setBounds(360, 221, 113, 15);
+		JLabel lblNameOfBank = new JLabel("Bank Name (*)");
+		lblNameOfBank.setBounds(381, 221, 113, 15);
 		contentPane.add(lblNameOfBank);
 
 		nameOfBankTextField = new JTextField();
-		nameOfBankTextField.setBounds(478, 219, 202, 19);
+		nameOfBankTextField.setBounds(503, 217, 202, 19);
 		contentPane.add(nameOfBankTextField);
 		nameOfBankTextField.setColumns(10);
 
-		JLabel lblEducation = new JLabel("Education");
-		lblEducation.setBounds(12, 250, 70, 15);
+		JLabel lblEducation = new JLabel("Education (*)");
+		lblEducation.setBounds(12, 250, 90, 15);
 		contentPane.add(lblEducation);
 
 		educationComboBox = new JComboBox();
 		educationComboBox.setModel(new DefaultComboBoxModel(EducationItems
 				.toArray()));
-		educationComboBox.setBounds(152, 248, 190, 19);
+		educationComboBox.setBounds(177, 246, 190, 19);
 		contentPane.add(educationComboBox);
 
 		JLabel lblDiploma = new JLabel("Diploma");
-		lblDiploma.setBounds(360, 250, 70, 15);
+		lblDiploma.setBounds(381, 250, 70, 15);
 		contentPane.add(lblDiploma);
 
 		diplomaTextField = new JTextField();
-		diplomaTextField.setBounds(478, 248, 202, 19);
+		diplomaTextField.setBounds(503, 246, 202, 19);
 		contentPane.add(diplomaTextField);
 		diplomaTextField.setColumns(10);
 
-		JLabel lblForeignLanguage = new JLabel("Foreign language");
+		JLabel lblForeignLanguage = new JLabel("Foreign Language");
 		lblForeignLanguage.setBounds(12, 279, 129, 15);
 		contentPane.add(lblForeignLanguage);
 
 		foreignLanguageTextField = new JTextField();
-		foreignLanguageTextField.setBounds(152, 277, 190, 19);
+		foreignLanguageTextField.setBounds(177, 275, 190, 19);
 		contentPane.add(foreignLanguageTextField);
 
 		JLabel lblCertificateOfIt = new JLabel("IT Certificate");
-		lblCertificateOfIt.setBounds(360, 279, 113, 15);
+		lblCertificateOfIt.setBounds(381, 279, 113, 15);
 		contentPane.add(lblCertificateOfIt);
 
 		certificateOfITcombobox = new JComboBox();
 		certificateOfITcombobox.setModel(new DefaultComboBoxModel(
 				ITCertificateItems.toArray()));
-		certificateOfITcombobox.setBounds(478, 277, 202, 19);
+		certificateOfITcombobox.setBounds(503, 275, 202, 19);
 		contentPane.add(certificateOfITcombobox);
 
 		JButton btnSave = new JButton("Save");
@@ -233,17 +233,22 @@ public class EmployeeFrame extends JFrame {
 				EmployeeController.singleton.addAction();
 			}
 		});
-		btnSave.setBounds(185, 333, 117, 25);
+		btnSave.setBounds(188, 347, 117, 25);
 		contentPane.add(btnSave);
 		
 		JButton btnCancel = new JButton("Cancel");
-		btnCancel.setBounds(360, 333, 117, 25);
+		btnCancel.setBounds(363, 347, 117, 25);
 		btnCancel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				EmployeeController.singleton.doCancel();
 			}
 		});
 		contentPane.add(btnCancel);
+		
+		JLabel lblRequiredFields = new JLabel("(*) This information is required");
+		lblRequiredFields.setForeground(Color.RED);
+		lblRequiredFields.setBounds(12, 308, 355, 15);
+		contentPane.add(lblRequiredFields);
 		setLocationRelativeTo(null);
 	}
 
